1ac5c2a28b9e8951f91a9c5ff9401578382dafd8
Fay Ho <498130877@qq.com>
2017-06-06 10:49:37 +0800

    Update

---
+++ Rust/closure/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    say(&|content: String| -> String {
+        println!("{}", content);
+        return "Hello Rust!".to_string();
+    })
+}
+
+fn say(reply: &Fn(String) -> String) {
+  println!("{}", reply("Hello World!".to_string()));
+}
