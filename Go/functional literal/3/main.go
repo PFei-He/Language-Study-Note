10cf75455587ebc91962438ef0867bf0e5872fb8
Fay Ho <498130877@qq.com>
2017-06-14 10:36:45 +0800

    Update

--- Go/functional literal/3/main.go
+++ Go/functional literal/3/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	say(func(content string) string {
+	say(func(content string)string {
 		fmt.Println(content)
 		return "Hello Go!"
 	})
 }
 
-func say(reply func(string) string)  {
+func say(reply func(string)string)  {
 	fmt.Println(reply("Hello World!"))
 }
