55c96800b7d9415175739337eb96e50e6fb0324f
Fay Ho <king.hepf@gmail.com>
2019-05-11 17:02:35 +0800

    Update

---
+++ C#/hash/Dictionary/1/main.cs
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2019 faylib.top
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System;
+using System.Collections.Generic;
+
+public class M {
+    public static void Main() {
+    	Dictionary<String, int> dictionary = new Dictionary<String, int>();
+        dictionary.Add("a", 1);
+        dictionary.Add("b", 2);
+        dictionary.Add("c", 3);
+		foreach (KeyValuePair<String, int> kvp in dictionary)
+		{
+			Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+		}
+		Console.WriteLine(dictionary["a"]);
+		Console.WriteLine(dictionary["b"]);
+		Console.WriteLine(dictionary["c"]);
+    }
+}
